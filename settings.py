3102c240d7c59c646ce8ffe49f7dd2b38a2ea214
Dantekk <pana.ciro0095@gmail.com>
2021-08-25 22:37:26 +0200

    l

---
+++ settings.py
@@ -0,0 +1,45 @@
+# Desired image dimensions
+IMAGE_SIZE = (299, 299)
+# Max vocabulary size
+MAX_VOCAB_SIZE = 2000000
+# Fixed length allowed for any sequence
+SEQ_LENGTH = 25
+# Dimension for the image embeddings and token embeddings
+EMBED_DIM = 512
+# Number of self-attention heads
+NUM_HEADS = 6
+# Per-layer units in the feed-forward network
+FF_DIM = 1024
+# Shuffle dataset dim on tf.data.Dataset
+SHUFFLE_DIM = 512
+# Batch size
+BATCH_SIZE = 64
+# Numbers of training epochs
+EPOCHS = 14
+
+# Reduce Dataset
+# If you want reduce number of train/valid images dataset, set 'REDUCE_DATASET=True'
+# and set number of train/valid images that you want.
+#### COCO dataset
+# Max number train dataset images : 68363
+# Max number valid dataset images : 33432
+REDUCE_DATASET = False
+# Number of train images -> it must be a value between [1, 68363]
+NUM_TRAIN_IMG = None
+# Number of valid images -> it must be a value between [1, 33432]
+NUM_VALID_IMG = None
+# Data augumention on train set
+TRAIN_SET_AUG = True
+# Data augumention on valid set
+VALID_SET_AUG = False
+
+
+# Load train_data.json pathfile
+train_data_json_path = "COCO_dataset/captions_mapping_train.json"
+# Load valid_data.json pathfile
+valid_data_json_path = "COCO_dataset/captions_mapping_valid.json"
+# Load text_data.json pathfile
+text_data_json_path  = "COCO_dataset/text_data.json"
+
+# Save training files directory
+SAVE_DIR = "save_train_dir/"
